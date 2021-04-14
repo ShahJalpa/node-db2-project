3311ceac13ff7f6682e211356db6da2c0289afa8
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-04-14 11:54:35 -0400

    spaces added using ctrl+alt+f

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -5,32 +5,32 @@ const vinValidator = require("vin-validator");
 
 const checkCarId = async (req, res, next) => {
   // DO YOUR MAGIC
-  try{
+  try {
     const carId = await Cars.getById(req.params.id);
-    if(!carId){
-      res.status(404).json({message: `car with id ${req.params.id} is not found`})
-    }else{
+    if (!carId) {
+      res.status(404).json({ message: `car with id ${req.params.id} is not found` })
+    } else {
       req.carId = carId
       next();
     }
-  }catch (error){
+  } catch (error) {
     next(error)
   }
 }
 
 const checkCarPayload = (req, res, next) => {
   // DO YOUR MAGIC
-  const {vin, make, model, milage} = req.body
+  const { vin, make, model, milage } = req.body
 
-  if(!vin){
-    res.status(400).json({message: "vin is missing"})
-  }else if(!make){
-    res.status(400).json({message: "make is missing"})
-  }else if(!model){
-    res.status(400).json({message: "model is missing"})
-  }else if(!milage){
-    res.status(400).json({message: "milage is missing"})
-  }else{
+  if (!vin) {
+    res.status(400).json({ message: "vin is missing" })
+  } else if (!make) {
+    res.status(400).json({ message: "make is missing" })
+  } else if (!model) {
+    res.status(400).json({ message: "model is missing" })
+  } else if (!milage) {
+    res.status(400).json({ message: "milage is missing" })
+  } else {
     next()
   }
 }
@@ -38,23 +38,23 @@ const checkCarPayload = (req, res, next) => {
 const checkVinNumberValid = (req, res, next) => {
   // DO YOUR MAGIC
   const validVinNum = vinValidator.validate(req.body.vin);
-  if(!validVinNum){
-    res.status(400).json({message: `vin ${req.body.vin} is not valid`})
-  }else{
+  if (!validVinNum) {
+    res.status(400).json({ message: `vin ${req.body.vin} is not valid` })
+  } else {
     next();
   }
 }
 
 const checkVinNumberUnique = async (req, res, next) => {
   // DO YOUR MAGIC
-  
-    const vinUnique = await db("cars").where("vin", req.body.vin.trim())
-    
-    if(vinUnique.length !=0){
-      res.status(400).json({message: `vin ${req.body.vin} is already exists`})
-    }else{
-      next();
-    }
+
+  const vinUnique = await db("cars").where("vin", req.body.vin.trim())
+
+  if (vinUnique.length != 0) {
+    res.status(400).json({ message: `vin ${req.body.vin} is already exists` })
+  } else {
+    next();
+  }
 }
 
 module.exports = {
